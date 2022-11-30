472c1801f07699584d1c4811a16401a2a6fddaaa
stephaniamatvei <stephania.matvei@isa.utm.md>
2022-11-30 11:10:49 +0200

    add some comments

--- src/main/java/md/utm/restservice/model/EmployeeModelAssembler.java
+++ src/main/java/md/utm/restservice/model/EmployeeModelAssembler.java
@@ -7,13 +7,18 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
+// class for simplifying link creation in my app
 @Component
 public class EmployeeModelAssembler implements RepresentationModelAssembler<Employee, EntityModel<Employee>> {
 
+    // converts a non-model object (Employee) into a model-based object (EntityModel<Employee>)
     @Override
     public EntityModel<Employee> toModel(Employee employee) {
         return EntityModel.of(employee,
+                /* asks that Spring HATEOAS build a link to the EmployeeController's getOneEmployee() method,
+                   and flag it as a self link */
                 linkTo(methodOn(EmployeeController.class).getOneEmployee(employee.getId())).withSelfRel(),
+                // asks Spring HATEOAS to build a link to the aggregate root, getAllEmployees(), and call it "employees"
                 linkTo(methodOn(EmployeeController.class).getAllEmployees()).withRel("employees"));
     }
 
